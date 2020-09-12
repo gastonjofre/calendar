eb79d283cf98886786e548fb01c25cb027897593
Gaston <gaston0018@gmail.com>
2020-09-12 02:28:09 -0300

    show actual month with last days from previous month and first days from next month - add constants for months and days

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CalendarView from './views/calendarView';
 
-const root = <div>{'Hello Calendar'}</div>;
+const root = <CalendarView />;
 
 ReactDOM.render(root, document.getElementById('root'));
