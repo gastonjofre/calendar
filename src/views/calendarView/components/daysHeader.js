26f7f0dc3375182f0c89d7dd227ba077bda5cb37
Gaston <gaston0018@gmail.com>
2020-09-15 11:48:52 -0300

    indent issues

--- src/views/calendarView/components/daysHeader.js
+++ src/views/calendarView/components/daysHeader.js
@@ -12,27 +12,39 @@ const styles = (theme) => ({
     background: theme.palette.primary.light,
   },
   shortTitle: {
-    display: "inline",
+    display: 'inline',
     [theme.breakpoints.up('md')]: {
-      display: "none",
+      display: 'none',
     },
   },
   longTitle: {
-    display: "none",
+    display: 'none',
     [theme.breakpoints.up('md')]: {
-      display: "inline",
+      display: 'inline',
     },
-  }
+  },
 });
 
 const DaysHeader = ({ classes }) =>
   days.map((day) => (
     <Card key={`header-${day.short}`} className={classes.root} variant="outlined">
       <Grid container direction="column" justify="center" alignItems="stretch">
-        <Typography variant="subtitle1" className={classes.longTitle} color="textSecondary" gutterBottom align="center">
+        <Typography
+          variant="subtitle1"
+          className={classes.longTitle}
+          color="textSecondary"
+          gutterBottom
+          align="center"
+        >
           {day.long}
         </Typography>
-        <Typography variant="body2" className={classes.shortTitle} color="textSecondary" gutterBottom align="center">
+        <Typography
+          variant="body2"
+          className={classes.shortTitle}
+          color="textSecondary"
+          gutterBottom
+          align="center"
+        >
           {day.short}
         </Typography>
       </Grid>
